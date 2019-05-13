8467cafd5af03346418fd3be4382fed7d83593b1
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-13 20:06:15 +0530

    Add.test case for input 7

--- src/test.sh
+++ src/test.sh
@@ -3,3 +3,5 @@ java BinarySearch 1 >actual
 diff expectedOutputForInput1 actual
 java BinarySearch 12 >actual
 diff expectedOutputForInput12 actual
+java BinarySearch 7 >actual
+diff expectedOutputForInput7 actual
