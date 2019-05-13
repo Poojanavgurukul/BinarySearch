b2910489644cfebfb1684e9be394e882e86d0763
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-13 19:09:17 +0530

    Add.implemented for loop and condition too

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -4,5 +4,12 @@ public class BinarySearch {
         Integer numberList[]={1,2,3,4,5,6,7,8,9,10,11};
         int highIndexNumber=numberList.length;
         int lowIndexNumber=0;
+        for (int i = 0; i <numberList.length ; i++) {
+            int middleIndexNumber=(highIndexNumber+lowIndexNumber)/2;
+            if(numberList[middleIndexNumber]==numberToBeSearch){
+                System.out.println(middleIndexNumber);
+                break;
+            }
+        }
     }
 }
