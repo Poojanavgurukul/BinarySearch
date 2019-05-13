e42bdd2835327b6a4a89a62ec97ee63181197d35
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-13 19:05:29 +0530

    Add.variables

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,6 +1,8 @@
 public class BinarySearch {
     public static void main(String[] args) {
         int numberToBeSearch=Integer.parseInt(args[0]);
-        Integer numberList[]={1,2,3,4,5,6,7,8,9,10};
+        Integer numberList[]={1,2,3,4,5,6,7,8,9,10,11};
+        int highIndexNumber=numberList.length;
+        int lowIndexNumber=0;
     }
 }
