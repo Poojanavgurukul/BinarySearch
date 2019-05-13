cbc6d476028ffc99ca242e9ff7e953f670285f2e
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-13 16:21:50 +0530

    Add.list of numbers

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,5 +1,6 @@
 public class BinarySearch {
     public static void main(String[] args) {
         int numberToBeSearch=Integer.parseInt(args[0]);
+        Integer numberList[]={1,2,3,4,5,6,7,8,9,10};
     }
 }
