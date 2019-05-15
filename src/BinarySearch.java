b83096d9d18f0ebd65dadc1186f67fb466e8fb1e
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-15 17:17:16 +0530

    Add.made a function for Binary search and added some variable too

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -2,21 +2,27 @@ public class BinarySearch {
     public static void main(String[] args) {
         int numberToBeSearch=Integer.parseInt(args[0]);
         Integer numberList[]={1,2,3,4,5,6,7,8,9,10,11};
-        int highIndexNumber=numberList.length;
-        int lowIndexNumber=0;
-        for (int i = 0; i <numberList.length ; i++) {
-            int middleIndexNumber=(highIndexNumber+lowIndexNumber)/2;
-            if(numberList[middleIndexNumber]==numberToBeSearch){
-                System.out.println(middleIndexNumber);
-                return;
+        int highIndexNumber=numberList.length-1;
+        int result=BinarySearch(numberList,0,highIndexNumber,numberToBeSearch);
+        if(result==-1){
+            System.out.println("Element is not found");
+        }else {
+            System.out.println(result);
+        }
+    }
+    public static int BinarySearch(Integer numberList[],int lowIndexNumber,int highIndexNumber,int numberToBeSearch){
+        if(highIndexNumber>=lowIndexNumber){
+            int middleindexnumber=lowIndexNumber+(highIndexNumber-lowIndexNumber)/2;
+            if(numberList[middleindexnumber]==numberToBeSearch){
+                return middleindexnumber;
             }
-            else if(numberList[middleIndexNumber]>numberToBeSearch){
-                highIndexNumber=middleIndexNumber;
+            else if(numberList[middleindexnumber]>numberToBeSearch){
+                return BinarySearch(numberList ,lowIndexNumber,middleindexnumber-1,numberToBeSearch );
             }
-            else if(numberList[middleIndexNumber]<numberToBeSearch){
-                lowIndexNumber=middleIndexNumber;
+            else {
+                return BinarySearch(numberList,middleindexnumber+1,highIndexNumber,numberToBeSearch);
             }
         }
-        System.out.println("Element is not found");
+        return -1;
     }
 }
