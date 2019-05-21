c40410cf8347bde78a058948b5d9171d476f9c79
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-21 09:20:11 +0530

    Refactor.change the variable and function of test

--- src/Test/java/binaryTest/BinaryTest.java
+++ src/Test/java/binaryTest/BinaryTest.java
@@ -9,14 +9,14 @@ import static org.junit.Assert.assertEquals;
 
 public class BinaryTest {
 
-    @Test public void numberInlist(){
-        Integer numberlist[]={1,2,3,4,5,6,7,8,9,10,11};
+    @Test public void numberInList(){
+        Integer numberList[]={1,2,3,4,5,6,7,8,9,10,11};
         int expexted=0;
-        assertEquals(expexted, BinarySearch.BinarySearch(1,numberlist,0,11));
+        assertEquals(expexted, BinarySearch.BinarySearch(1,numberList,0,11));
     }
     @Test public void numberNotInList(){
-        Integer numberlist[]={10,20,30,40,50,60,70,80};
+        Integer numberList[]={10,20,30,40,50,60,70,80};
         int expected=-1;
-        assertEquals(expected,BinarySearch.BinarySearch(90,numberlist,0,7));
+        assertEquals(expected,BinarySearch.BinarySearch(90,numberList,0,7));
     }
 }
