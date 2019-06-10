35f311ddab18dd340bb67759b28452b6fc5248f7
Poojanavgurukul <Pooja17@navgurukul.org>
2019-06-10 14:48:26 +0530

    Refactor.make it in a genre

--- src/Test/java/binaryTest/BinaryTest.java
+++ src/Test/java/binaryTest/BinaryTest.java
@@ -3,6 +3,7 @@ package binaryTest;
 import binary.BinarySearch;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -10,12 +11,12 @@ import static org.junit.Assert.assertEquals;
 public class BinaryTest {
 
     @Test public void numberInList(){
-        Integer numberList[]={1,2,3,4,5,6,7,8,9,10,11};
+        List<Integer>numberList= Arrays.asList(1,2,3,4,5,6,7,8,9,10,11);
         int expexted=0;
         assertEquals(expexted, BinarySearch.BinarySearch(1,numberList,0,11));
     }
     @Test public void numberNotInList(){
-        Integer numberList[]={10,20,30,40,50,60,70,80};
+        List<Integer>numberList= Arrays.asList(10,20,30,40,50,60,70,80);
         int expected=-1;
         assertEquals(expected,BinarySearch.BinarySearch(90,numberList,0,7));
     }
--- src/main/java/binary/BinarySearch.java
+++ src/main/java/binary/BinarySearch.java
@@ -1,33 +1,39 @@
 package binary;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BinarySearch {
     public static void main(String[] args) {
         int numberToBeSearch=Integer.parseInt(args[0]);
-        Integer numberList[]={1,2,3,4,5,6,7,8,9,10,11};
-        int highIndexNumber=numberList.length-1;
-        int result=BinarySearch(numberToBeSearch,numberList,0,highIndexNumber);
+        List<Integer>elementList= Arrays.asList(1,2,3,4,5,6,7,8,9,10,11);
+        int highIndexNumber=elementList.size()-1;
+        int result=BinarySearch(numberToBeSearch,elementList,0,highIndexNumber);
         if(result==-1){
             System.out.println("Element is not found");
         }else {
             System.out.println(result);
         }
     }
-    public static int BinarySearch(int numberToBeSearch,Integer[] numberList, int lowIndexNumber, int highIndexNumber){
+    public static <T extends Comparable<T>>int BinarySearch(T elementToBeSearch,List<T> elementList, int lowIndexNumber, int highIndexNumber){
         if(highIndexNumber>=lowIndexNumber){
             int middleindexnumber=lowIndexNumber+(highIndexNumber-lowIndexNumber)/2;
-            if(isNumberFound(numberToBeSearch, numberList[middleindexnumber])){
+            if(isAElement(elementToBeSearch, elementList, middleindexnumber)){
                 return middleindexnumber;
             }
-            else if(numberList[middleindexnumber]>numberToBeSearch){
-                return BinarySearch(numberToBeSearch,numberList ,lowIndexNumber,middleindexnumber-1 );
+            else if(elementList.get(middleindexnumber).compareTo(elementToBeSearch)>0){
+                return BinarySearch(elementToBeSearch,elementList ,lowIndexNumber,middleindexnumber-1 );
             }
             else {
-                return BinarySearch(numberToBeSearch,numberList,middleindexnumber+1,highIndexNumber);
+                return BinarySearch(elementToBeSearch,elementList,middleindexnumber+1,highIndexNumber);
             }
         }
         return -1;
     }
 
-    private static boolean isNumberFound(int numberToBeSearch, Integer integer) {
-        return integer == numberToBeSearch;
+    private static <T extends Comparable<T>> boolean isAElement(T elementToBeSearch, List<T> elementList, int middleindexnumber) {
+        return elementList.get(middleindexnumber)==elementToBeSearch;
     }
+
 }
